ffe1d723a52e39d2b896c9fa62ba96d354eff255
Mike <lvlichaelly@gmail.com>
2016-02-04 18:35:59 -0800

    basic setup

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  // $('.main__splash-img').click(function() {
+  //   console.log('sending email');
+  //   window.open('mailto:lvlichaelly@gmail.com?subject=testemail&body=hello');
+  // })
+})
